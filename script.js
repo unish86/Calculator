019b2a9f36cfab91bdf20567b6e5d17439c78278
unish86 <prajapatiunish63@gmail.com>
2025-08-02 21:59:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendValue(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function deleteLast() {
+  const display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    const result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch (error) {
+    alert("Invalid Expression");
+  }
+}
